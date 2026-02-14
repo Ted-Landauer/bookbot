98b20500040d348b6bac58369c3787618378ec14
Ted Landauer <tedlandauer@gmail.com>
2026-02-13 17:42:43 -0800

    final addition to the bookbot. Dynamic linking to books instead of hard coding

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
 from stats import wordCount, characterCount, sortedDictionary
-#from stats import characterCount
+
 
 
 #reads in a book when provided with a file path
@@ -13,35 +14,44 @@ def get_book_text(filePath):
 #run it all
 def main():
     
-    bookLocation = "books/frankenstein.txt"
+    bookLocationHardCoded = "books/frankenstein.txt"
+    bookLocationDynamic = ""
     
-    #print the book out
-    #print(get_book_text("./books/frankenstein.txt"))
-    
-    #print the wordcount of a book
-    #print("Found " + str(wordCount(get_book_text("./books/frankenstein.txt"))) + " total words")
-    
-    #print the character count of a book
-    #print(characterCount(get_book_text("./books/frankenstein.txt")))
-    
-    #print the sorted character count of the book with alpha characters only
-    #print(sortedDictionary(characterCount(get_book_text("./books/frankenstein.txt"))))
-    
-    #run the function to make formatting the output later a little nicer
-    sortedData = sortedDictionary(characterCount(get_book_text("./" + bookLocation)))
-    
-    
-    #print the final output
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at " + str(bookLocation) + "...")
-    print("----------- Word Count ----------")
-    print("Found " + str(wordCount(get_book_text("./" + bookLocation))) + " total words")
-    print("--------- Character Count -------")
-    
-    for char, count in sortedData.items():
-        print(char + ": " + str(count))
+    if len(sys.argv) != 2:
         
-    print("============= END ===============")
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    else:
+        bookLocationDynamic = sys.argv[1]
+    
+        #print the book out
+        #print(get_book_text("./books/frankenstein.txt"))
+        
+        #print the wordcount of a book
+        #print("Found " + str(wordCount(get_book_text("./books/frankenstein.txt"))) + " total words")
+        
+        #print the character count of a book
+        #print(characterCount(get_book_text("./books/frankenstein.txt")))
+        
+        #print the sorted character count of the book with alpha characters only
+        #print(sortedDictionary(characterCount(get_book_text("./books/frankenstein.txt"))))
+        
+        #run the function to make formatting the output later a little nicer
+        sortedData = sortedDictionary(characterCount(get_book_text(bookLocationDynamic)))
+        
+        
+        #print the final output
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at " + str(bookLocationDynamic) + "...")
+        print("----------- Word Count ----------")
+        print("Found " + str(wordCount(get_book_text(bookLocationDynamic))) + " total words")
+        print("--------- Character Count -------")
+        
+        for char, count in sortedData.items():
+            print(char + ": " + str(count))
+            
+        print("============= END ===============")
 
     
     
